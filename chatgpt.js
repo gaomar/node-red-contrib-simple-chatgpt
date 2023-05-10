05aff080480c7c77d73795f56b0ce25b433b7065
Ryan Bennett <ryan@filament.com>
2023-05-10 14:57:50 -0600

    Update node creation example to support MacOS (#3)
    * update node creation example to support MacOS
    
    * add msg to error so that we can use catch blocks
    
    ---------
    
    Co-authored-by: Ryan Bennett <your@mail.com>

--- chatgpt.js
+++ chatgpt.js
@@ -1,9 +1,12 @@
 module.exports = (RED) => {
-    const {
-        Configuration,
-        OpenAIApi
-    } = require("openai");
-    const ACCEPT_TOPIC_LIST = ["completion", "image", "edit", "turbo", "gpt4"].map(item => item.toLowerCase());
+    const { Configuration, OpenAIApi } = require("openai");
+    const ACCEPT_TOPIC_LIST = [
+        "completion",
+        "image",
+        "edit",
+        "turbo",
+        "gpt4",
+    ].map((item) => item.toLowerCase());
     const main = function (config) {
         const node = this;
         RED.nodes.createNode(node, config);
@@ -16,40 +19,47 @@ module.exports = (RED) => {
         if (config.BaseUrl) {
             try {
                 const url = new URL(config.BaseUrl);
-                if (url.pathname === '/') {
-                    url.pathname = '/v1';
+                if (url.pathname === "/") {
+                    url.pathname = "/v1";
                 }
                 configuration.basePath = url.toString();
             } catch {
                 node.status({
                     fill: "red",
                     shape: "dot",
-                    text: `BaseUrl(${config.BaseUrl}) isn't a valid url`
+                    text: `BaseUrl(${config.BaseUrl}) isn't a valid url`,
                 });
             }
         }
         const openai = new OpenAIApi(configuration);
 
-        node.on('input', async(msg) => {
+        node.on("input", async (msg) => {
             node.status({
                 fill: "green",
                 shape: "dot",
-                text: "Processing..."
+                text: "Processing...",
             });
             if (config.topic != "__EMPTY__") {
                 msg.topic = config.topic;
-            };
+            }
             if (msg.topic) {
-                 msg.topic = msg.topic.toLowerCase();
-            };
-            if (!ACCEPT_TOPIC_LIST.includes(msg.topic) && msg.topic !== "__empty__") {
+                msg.topic = msg.topic.toLowerCase();
+            }
+            if (
+                !ACCEPT_TOPIC_LIST.includes(msg.topic) &&
+                msg.topic !== "__empty__"
+            ) {
                 node.status({
                     fill: "red",
                     shape: "dot",
-                    text: "msg.topic is incorrect"
+                    text: "msg.topic is incorrect",
                 });
-                node.error(`msg.topic must be a string set to one of the following values: ${ACCEPT_TOPIC_LIST.map(item => `'${item}'`).join(", ")}`)
-                node.send(msg)
+                node.error(
+                    `msg.topic must be a string set to one of the following values: ${ACCEPT_TOPIC_LIST.map(
+                        (item) => `'${item}'`
+                    ).join(", ")}`
+                );
+                node.send(msg);
             } else if (msg.topic === "image") {
                 try {
                     const response = await openai.createImage({
@@ -68,20 +78,20 @@ module.exports = (RED) => {
                     node.status({
                         fill: "blue",
                         shape: "dot",
-                        text: "Response complete"
+                        text: "Response complete",
                     });
-                    node.send(msg)
+                    node.send(msg);
                 } catch (error) {
                     node.status({
                         fill: "red",
                         shape: "dot",
-                        text: "Error"
+                        text: "Error",
                     });
                     if (error.response) {
-                        node.error(error.response.status);
-                        node.error(error.response.data);
+                        node.error(error.response.status, msg);
+                        node.error(error.response.data, msg);
                     } else {
-                        node.error(error.message);
+                        node.error(error.message, msg);
                     }
                 }
             } else if (msg.topic === "edit") {
@@ -92,37 +102,36 @@ module.exports = (RED) => {
                         n: parseInt(msg.n) || 1,
                         input: msg.last || "",
                         temperature: parseInt(msg.temperature) || 1,
-                        top_p: parseInt(msg.top_p) || 1
+                        top_p: parseInt(msg.top_p) || 1,
                     });
                     msg.payload = response.data.choices[0].text;
                     msg.full = response;
                     node.status({
                         fill: "blue",
                         shape: "dot",
-                        text: "Response complete"
+                        text: "Response complete",
                     });
-                    node.send(msg)
+                    node.send(msg);
                 } catch (error) {
                     node.status({
                         fill: "red",
                         shape: "dot",
-                        text: "Error"
+                        text: "Error",
                     });
                     if (error.response) {
-                        node.error(error.response.status);
-                        node.error(error.response.data);
+                        node.error(error.response.status, msg);
+                        node.error(error.response.data, msg);
                     } else {
-                        node.error(error.message);
+                        node.error(error.message, msg);
                     }
                 }
             } else if (msg.topic === "turbo") {
                 try {
-                    if (typeof msg.history === "undefined")
-                        msg.history = [];
+                    if (typeof msg.history === "undefined") msg.history = [];
                     msg.topic = "turbo";
                     const input = {
-                        "role": "user",
-                        "content": msg.payload
+                        role: "user",
+                        content: msg.payload,
                     };
                     msg.history.push(input);
                     const response = await openai.createChatCompletion({
@@ -135,12 +144,13 @@ module.exports = (RED) => {
                         stop: msg.stop || null,
                         max_tokens: parseInt(msg.max_tokens) || 4000,
                         presence_penalty: parseInt(msg.presence_penalty) || 0,
-                        frequency_penalty: parseInt(msg.frequency_penalty) || 0
+                        frequency_penalty: parseInt(msg.frequency_penalty) || 0,
                     });
-                    const trimmedContent = response.data.choices[0].message.content.trim();
+                    const trimmedContent =
+                        response.data.choices[0].message.content.trim();
                     const result = {
-                        "role": "assistant",
-                        "content": trimmedContent
+                        role: "assistant",
+                        content: trimmedContent,
                     };
                     msg.history.push(result);
                     msg.payload = response.data.choices[0].message.content;
@@ -148,30 +158,29 @@ module.exports = (RED) => {
                     node.status({
                         fill: "blue",
                         shape: "dot",
-                        text: "Response complete"
+                        text: "Response complete",
                     });
-                    node.send(msg)
+                    node.send(msg);
                 } catch (error) {
                     node.status({
                         fill: "red",
                         shape: "dot",
-                        text: "Error"
+                        text: "Error",
                     });
                     if (error.response) {
-                        node.error(error.response.status);
-                        node.error(error.response.data);
+                        node.error(error.response.status, msg);
+                        node.error(error.response.data, msg);
                     } else {
-                        node.error(error.message);
+                        node.error(error.message, msg);
                     }
                 }
             } else if (msg.topic === "gpt4") {
                 try {
-                    if (typeof msg.history === "undefined")
-                        msg.history = [];
+                    if (typeof msg.history === "undefined") msg.history = [];
                     msg.topic = "gpt4";
                     const input = {
-                        "role": "user",
-                        "content": msg.payload
+                        role: "user",
+                        content: msg.payload,
                     };
                     msg.history.push(input);
                     const response = await openai.createChatCompletion({
@@ -184,12 +193,13 @@ module.exports = (RED) => {
                         stop: msg.stop || null,
                         max_tokens: parseInt(msg.max_tokens) || 4000,
                         presence_penalty: parseInt(msg.presence_penalty) || 0,
-                        frequency_penalty: parseInt(msg.frequency_penalty) || 0
+                        frequency_penalty: parseInt(msg.frequency_penalty) || 0,
                     });
-                    const trimmedContent = response.data.choices[0].message.content.trim();
+                    const trimmedContent =
+                        response.data.choices[0].message.content.trim();
                     const result = {
-                        "role": "assistant",
-                        "content": trimmedContent
+                        role: "assistant",
+                        content: trimmedContent,
                     };
                     msg.history.push(result);
                     msg.payload = response.data.choices[0].message.content;
@@ -197,20 +207,20 @@ module.exports = (RED) => {
                     node.status({
                         fill: "blue",
                         shape: "dot",
-                        text: "Response complete"
+                        text: "Response complete",
                     });
-                    node.send(msg)
+                    node.send(msg);
                 } catch (error) {
                     node.status({
                         fill: "red",
                         shape: "dot",
-                        text: "Error"
+                        text: "Error",
                     });
                     if (error.response) {
-                        node.error(error.response.status);
-                        node.error(error.response.data);
+                        node.error(error.response.status, msg);
+                        node.error(error.response.data, msg);
                     } else {
-                        node.error(error.message);
+                        node.error(error.message, msg);
                     }
                 }
             } else {
@@ -230,34 +240,36 @@ module.exports = (RED) => {
                         stop: msg.stop || null,
                         presence_penalty: parseInt(msg.presence_penalty) || 0,
                         frequency_penalty: parseInt(msg.frequency_penalty) || 0,
-                        best_of: parseInt(msg.best_of) || 1
+                        best_of: parseInt(msg.best_of) || 1,
                     });
                     msg.payload = response.data.choices[0].text;
                     msg.full = response;
                     node.status({
                         fill: "blue",
                         shape: "dot",
-                        text: "Response complete"
+                        text: "Response complete",
                     });
-                    node.send(msg)
+                    node.send(msg);
                 } catch (error) {
                     node.status({
                         fill: "red",
                         shape: "dot",
-                        text: "Error"
+                        text: "Error",
                     });
                     if (error.response) {
-                        node.error(error.response.status);
-                        node.error(error.response.data);
+                        node.error(error.response.status, msg);
+                        node.error(error.response.data, msg);
                     } else {
-                        node.error(error.message);
+                        node.error(error.message, msg);
                     }
                 }
             }
         });
         // clear the node status(invalid option tips)
-        node.on('close', () => { node.status({}); });
-    }
+        node.on("close", () => {
+            node.status({});
+        });
+    };
 
     RED.nodes.registerType("chatgpt", main);
-}
+};
