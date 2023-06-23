7064570a2b0271f3cebe66a1723c6aae72f06b99
h-takauma <h-takauma@i-enter.co.jp>
2023-06-23 16:28:04 +0900

    FunctionCall対応

--- README.md
+++ README.md
@@ -11,6 +11,116 @@ or
 
 AdminタブからInstall
 
+## 使い方
+### 入力項目
+
+|項目|説明|
+|--|--|
+|Token|OpenAPIのAPIキーを設定します。|
+|Model|使用するモデル名を指定します。デフォルトは`gpt-3.5-turbo`です。|
+|SystemSetting|AIアシスタントの設定などを記述します。例えば、語尾に必ず「テヘペロ」とつけて等を指定することができます。|
+|pastMessages|会話の履歴を渡します。会話を継続する際に必要です。|
+|functions|`gpt-3.5-turbo-0613`以降から使えます。指定サンプルは別途。|
+|function_call|functionsで指定した関数名を強制実行できます。`auto`とすると自動的に判別して関数が呼ばれます。`none`だと呼ばれません。`{name: 関数名}`を指定すると強制的に対象関数を実行します。|
+
+### 出力項目
+
+|項目|説明|
+|--|--|
+|payload|ChatGPTの返答が返ってきます。Function実行された場合は`null`が返ります。|
+|pastMessages|会話の履歴配列を返します。|
+|payloadFunction|FunctionCallingで実行され場合、実行した関数名とJSONパース済みのargumentsが返ります。|
+
+## Functions指定方法
+Functionsのサンプルは下記の通りです。配列形式で関数名や関数の詳細、パラメータを指定します。
+
+|項目|説明|
+|--|--|
+|name|関数の名前。好きな名前を決めることができます。|
+|description|関数の詳細説明。ある程度詳細に書くほうが望ましいです。|
+|parameters.properties|パラメータの詳細。設定してほしいプロパティ名と型、説明文を記載します。|
+|parameters.required|プロパティで返してほしい必須のプロパティ名を指定します。|
+
+```json
+[
+    {
+        "name": "get_weather",
+        "description": "指定された場所と日付の天気を取得する",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "都道府県や市、町の名前, e.g. 東京都文京区"
+                },
+                "date": {
+                    "type": "string",
+                    "description": "Date formatted in YYYY/MM/DD, e.g. 2023/06/13"
+                }
+            },
+            "required": [
+                "location",
+                "date"
+            ]
+        }
+    },
+    {
+        "name": "recommend_book",
+        "description": "おすすめの本を1冊紹介する",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "title": {
+                    "type": "string",
+                    "description": "本のタイトル"
+                },
+                "description": {
+                    "type": "string",
+                    "description": "本の内容"
+                }
+            },
+            "required": [
+                "title",
+                "description"
+            ]
+        }
+    },
+    {
+        "name": "hashtag_text",
+        "description": "ユーザから与えられたテキストからハッシュタグを出力してください。",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "tag": {
+                    "type": "string",
+                    "description": "ハッシュタグを最低でも3つ以上出力してください。"
+                }
+            },
+            "required": [
+                "tag"
+            ]
+        }
+    }
+]
+```
+
+指定方法の詳細は[こちら](https://openai.com/blog/function-calling-and-other-api-updates)をご確認ください。
+
+## function_call指定方法
+文字列で`auto`か`none`の他に、Jsonオブジェクトで強制的に実行する関数名を指定します。
+上記Functionsを与えた場合の例です。`hashtag_text`が強制的に行われるようになります。
+
+```json
+{
+    "name": "hashtag_text"
+}
+```
+
+## 設定例
+
+![s100](images/s100.png)
+
+
 ## LINK
 
 * [NodeRED](https://flows.nodered.org/node/node-red-contrib-simple-chatgpt)
@@ -19,6 +129,7 @@ AdminタブからInstall
 
 ## release
 
+* 2023/06/23: Function Calling対応（v1.2.0）
 * 2023/03/02: ChatGPT API（gpt-3.5-turbo）対応（v1.1.0）
 * 2022/12/05: 初回リリース（v1.0.0）
 
--- node.js
+++ node.js
@@ -1,35 +1,108 @@
 module.exports = (RED) => {
   const { Configuration, OpenAIApi } = require("openai");
-  const main = function(config) {
-      const node = this;
-      RED.nodes.createNode(node, config);
-      const TOKEN = config.Token;
+  const main = function (config) {
+    RED.nodes.createNode(this, config);
+    this.functions = config.functions || [];
+    this.functionsType = config.functionsType || "str";
+    this.function_call = config.function_call || "auto";
+    this.function_callType = config.function_callType || "str";
+    this.Token = config.Token || "";
+    const node = this;
 
-      const configuration = new Configuration({
-        apiKey: TOKEN,
-      });
-      const openai = new OpenAIApi(configuration);
+    const configuration = new Configuration({
+      apiKey: node.Token,
+    });
+    const openai = new OpenAIApi(configuration);
 
-      node.on('input', async (msg) => {
-        node.status({fill:"green",shape:"dot",text:"処理中..."});
-        if (typeof msg.pastMessages === "undefined") msg.pastMessages = [];
-        if (msg.pastMessages.length == 0 && config.SystemSetting.length != 0) {
-          const system = {"role": "system", "content": config.SystemSetting};
-          msg.pastMessages.push(system);
+    node.on("input", async (msg) => {
+      node.status({ fill: "green", shape: "dot", text: "処理中..." });
+      if (typeof msg.pastMessages === "undefined") msg.pastMessages = [];
+      if (msg.pastMessages.length == 0 && config.SystemSetting.length != 0) {
+        const system = { role: "system", content: config.SystemSetting };
+        msg.pastMessages.push(system);
+      }
+      const newMessage = { role: "user", content: msg.payload };
+      msg.pastMessages.push(newMessage);
+
+      var functions;
+      var function_call;
+      RED.util.evaluateNodeProperty(
+        node.functions,
+        node.functionsType,
+        node,
+        msg,
+        (err, value) => {
+          if (err) {
+            return;
+          } else {
+            functions = value;
+          }
         }
-        const newMessage = {"role": "user", "content": msg.payload};
-        msg.pastMessages.push(newMessage);
-        const response = await openai.createChatCompletion({
-          model: 'gpt-3.5-turbo',
-          messages: msg.pastMessages
-        });
-        const responseMessage = {"role": "assistant", "content": response.data.choices[0].message.content};
+      );
+      RED.util.evaluateNodeProperty(
+        node.function_call,
+        node.function_callType,
+        node,
+        msg,
+        (err, value) => {
+          if (err) {
+            return;
+          } else {
+            function_call = value;
+          }
+        }
+      );
+
+      const params = {
+        model: config.Model.length == 0 ? "gpt-3.5-turbo" : config.Model,
+        messages: msg.pastMessages,
+        functions,
+        function_call,
+      };
+      if (params.functions.length == 0) {
+        delete params.functions;
+        delete params.function_call;
+      }
+      try {
+        const response = await openai.createChatCompletion(params);
+        const responseMessage = {
+          role: "assistant",
+          content: response.data.choices[0].message.content,
+        };
         msg.pastMessages.push(responseMessage);
         msg.payload = response.data.choices[0].message.content;
+        if (response.data.choices[0].message?.function_call) {
+          const functionData = response.data.choices[0].message.function_call;
+          msg.payloadFunction = {
+            name: functionData.name,
+            function_call: JSON.parse(functionData.arguments),
+          };
+        }
         node.status({});
-        node.send(msg)
-      });
-  }
+      } catch (error) {
+        if (error.response) {
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `${error.response.status}, ${JSON.stringify(
+              error.response.data.error.message
+            )}`,
+          });
+          msg.payload = `${error.response.status}, ${JSON.stringify(
+            error.response.data.error.message
+          )}`;
+        } else {
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: `${error.type}, ${error.message}`,
+          });
+          msg.payload = `${error.type}, ${error.message}`;
+        }
+      }
+      node.send(msg);
+    });
+  };
 
   RED.nodes.registerType("simple-chatgpt", main);
-}
+};
